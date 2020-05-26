9aa6f760969efcf1c3f8de7a8486fc8c1802362a
chisoftltd <chisoftltd@gmail.com>
2020-05-26 18:26:21 +0200

    Open a File on the Server. # Read Only Parts of the File. # Read Lines. # Close Files

---
+++ PythonWriteFile.py
@@ -0,0 +1,30 @@
+# Write to an Existing File
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content! TXT files are useful for storing information in plain text with no special formatting beyond basic fonts and font styles.")
+f.close()
+
+myfile = open("Tutorial2.txt", "w")
+
+myfile.write("Python Programming Tutorials \n")
+myfile.write("Coding Challenge \n")
+myfile.write("Java Programming Tutorial")
+
+
+myfile.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+
+f = open("demofile.txt", "w")
+f.write("Woops! I have deleted the content! The file is commonly used for recording notes, directions, and other similar documents that do not need to appear a certain way. If you are looking to create a document with more formatting capabilities, such as a report, newsletter, or resume, you should look to the .DOCX file, which is used by the popular Microsoft Word program.")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile.txt", "r")
+print(f.read())
+
+# Create a New File
+f = open("myfile4.txt", "x")
+f = open("myfile5.txt", "w")
